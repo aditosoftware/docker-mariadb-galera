b519125814970e466f12c3f251660fa9258d717b
Fryderyk Dziarmagowski <f.dziarmagowski@adito.de>
2020-06-28 16:47:45 +0200

    - debianized, rework Dockerfile and shell scripts

--- report_status.sh
+++ report_status.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+
+# shellcheck disable=SC2086
+set -x
+
 # Report Galera status to etcd periodically.
 # report_status.sh [mysql user] [mysql password] [cluster name] [interval] [comma separated etcd hosts]
 # Example: 
@@ -8,49 +12,36 @@ USER=$1
 PASSWORD=$2
 CLUSTER_NAME=$3
 TTL=$4
-ETCD_HOSTS=$5
+ETCD_CLUSTER=$5
 
-function check_etcd()
-{
-  etcd_hosts=$(echo $ETCD_HOSTS | tr ',' ' ')
-  flag=1
-
-  # Loop to find a healthy etcd host
-  for i in $etcd_hosts
-  do
-    curl -s http://$i/health > /dev/null || continue
-    if curl -s http://$i/health | jq -e 'contains({ "health": "true"})' > /dev/null; then
-      healthy_etcd=$i
-      flag=0
-      break
+check_etcd() {
+    curl -s http://$ETCD_CLUSTER/health > /dev/null
+    if curl -s http://$ETCD_CLUSTER/health | jq -e 'contains({ "health": "true"})' > /dev/null; then
+        flag=0
     fi
-  done
-
-  # Flag is 0 if there is a healthy etcd host
-  [ $flag -ne 0 ] && echo "report>> Couldn't reach healthy etcd nodes."
+    # Flag is 0 if there is a healthy etcd host
+    [ $flag -ne 0 ] && echo "report>> Couldn't reach healthy etcd nodes."
 }
 
-function report_status()
-{
+report_status() {
   var=$1
   key=$2
 
-  if [ ! -z $var ]; then
+  if [ -n "$var" ]; then
     check_etcd
-    
-    URL="http://$healthy_etcd/v2/keys/galera/$CLUSTER_NAME"
+
+    URL="http://$ETCD_CLUSTER/v2/keys/galera/$CLUSTER_NAME"
     output=$(mysql --user=$USER --password=$PASSWORD -A -Bse "show status like '$var'" 2> /dev/null)
     if [ -z $key ]; then
-      key=$(echo $output | awk {'print $1'})
+      key=$(echo $output | awk '{ print $1 }')
     fi
-    value=$(echo $output | awk {'print $2'})
-    ipaddr=$(hostname -i | awk {'print $1'})
+    value=$(echo $output | awk '{ print $2 }')
+    ipaddr=$(hostname -i | awk '{ print $1 }')
 
-    if [ ! -z $value ]; then
-      curl -s $URL/$ipaddr/$key -X PUT -d "value=$value&ttl=$TTL" > /dev/null
+    if [ -n "$value" ]; then
+      curl -s "$URL/$ipaddr/$key" -X PUT -d "value=$value&ttl=$TTL" > /dev/null
     fi
   fi
-  
 }
 
 while true;
@@ -58,5 +49,5 @@ do
   report_status wsrep_local_state_comment
   report_status wsrep_last_committed seqno
   # report every ttl - 2 to ensure value does not expire
-  sleep $(($TTL - 2))
+  sleep $((TTL - 2))
 done
