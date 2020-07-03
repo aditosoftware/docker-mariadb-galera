10fd1843985820ce0155eb8a69bf49740085809b
Fryderyk Dziarmagowski <f.dziarmagowski@adito.de>
2020-07-03 13:56:18 +0200

    - drop health checks, provide only liveness and readiness probes

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,66 +1,42 @@
 #!/bin/sh
 
-status() {
-    var=$1
-    mysql --user=root --password="$MYSQL_ROOT_PASSWORD" -ABse "SHOW GLOBAL STATUS LIKE '${var}';" | awk '{ print $2 }'
-}
-
 galera_cnf() {
     grep "$1" /etc/mysql/conf.d/galera.cnf | awk -F' = ' '{ print $2 }'
 }
 
 is_ready() {
-    # Return OK when container is still initializing
-    if [ -S /var/run/mysqld/mysqld.sock ]; then
-        if [ -f /tmp/entrypoint.init ]; then
-            if [ "$(status wsrep_ready)" ]; then
-                rm -f /tmp/entrypoint.init
-            else
-                exit 0
-            fi
-        fi
+    # check for mysqld socket
+    if [ ! -S /var/run/mysqld/mysqld.sock ]; then
+        exit 1
+    fi
+    # check mysqld status
+    if ! mysqladmin status --user=root --password="$MYSQL_ROOT_PASSWORD" >/dev/null; then
+        exit 1
+    fi
+    # check galera status
+    if ! status wsrep_ready; then
+        exit 1
+    fi
+    # entrypoint is finished at this point
+    if [ -f /tmp/entrypoint.init ]; then
+        rm -f /tmp/entrypoint.init
     fi
 }
 
-health_check() {
-    is_ready
-    ready=$(status wsrep_ready)
-    connected=$(status wsrep_connected)
-    state=$(status wsrep_local_state_comment)
-
-    if [ "$(status wsrep_ready)" = "ON" ] && \
-        [ "$(status wsrep_connected)" = "ON" ] && \
-        [ "$(status wsrep_local_state_comment)" = "Synced" ]; then
-            exit 0
-        else
-            echo ">> Healthcheck failed"
-            echo "wsrep_ready: $ready"
-            echo "wsrep_connected: $connected"
-            echo "wsrep_local_state_comment: $state"
-            exit 1
-    fi
-}
-
-health_check_verbose() {
-    echo "Galera node info:"
-    echo "  Name: $(galera_cnf wsrep_node_name)"
-    echo "  IPv4 address: $(galera_cnf wsrep_node_address)"
-    is_ready
-    echo ">> wsrep_cluster_address:      $(galera_cnf wsrep_cluster_address)"
-    echo ">> wsrep_ready:                $(status wsrep_ready)"
-    echo ">> wsrep_connected:            $(status wsrep_connected)"
-    echo ">> wsrep_local_state_comment:  $(status wsrep_local_state_comment)"
-    echo ">> wsrep_local_send_queue_avg: $(status wsrep_local_send_queue_avg)"
-    if [ "$(wsrep_flow_control_paused 2>/dev/null)" ]; then
-        echo ">> wsrep_flow_control_paused:  $(wsrep_flow_control_paused)"
+is_alive() {
+    if ! mysqladmin status --user=root --password="$MYSQL_ROOT_PASSWORD" >/dev/null; then
+        exit 1
     fi
 }
 
 case "$1" in
-    --verbose|-v)
-        health_check_verbose
+    --readiness)
+        is_ready
+        ;;
+    --liveness)
+        is_alive
         ;;
     *)
-        health_check
         ;;
 esac
+exit 0
